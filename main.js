a1d546619f70a18460c781deccfab60ef9d8354e
Mikheil Gabashvili <mikheilgabashvili@Mikheils-MacBook-Air.local>
2023-04-13 14:26:40 -0400

    Added pose images and set up varaibles for Kinect interaction in main

--- main.js
+++ main.js
@@ -28,11 +28,71 @@ var frames = {
         frames.socket = new WebSocket(url);
         frames.socket.onmessage = function (event) {
             frames.show(JSON.parse(event.data));
+            // var command = frames.get_pose_command(JSON.parse(event.data));
+            // if (command !== null) {
+            //     sendPoseCommand(command);
+            // }
         }
+
     },
+
+    // get_pose_command: function (frame) {
+    //     var command = null;
+    //     if (frame.people.length < 1) {
+    //       return command;
+    //     }
+        
+    //     // Normalize by subtracting the spine_chest joint coordinates
+    //     var spine_chest_x = frame.people[0].joints[2].position.x;
+    //     var spine_chest_y = frame.people[0].joints[2].position.y;
+    //     var spine_chest_z = frame.people[0].joints[2].position.z;
+
+    //     var left_wrist_x = (frame.people[0].joints[7].position.x - spine_chest_x) * -1;
+    //     var left_wrist_y = (frame.people[0].joints[7].position.y - spine_chest_y) * -1;
+       
+    //     var right_wrist_x = (frame.people[0].joints[14].position.x - spine_chest_x) * -1;
+    //     var right_wrist_y = (frame.people[0].joints[14].position.y - spine_chest_y) * -1;
+
+    //     if (left_wrist_y < 0 && right_wrist_y < 0) {
+    //         return command;
+    //     }
+
+    //     if (left_wrist_y > 400 && right_wrist_y > 400) { 
+    //         command = 73; //Pose 3
+    //     }
+
+    //     if (left_wrist_y > 0 && right_wrist_y < -400) { 
+    //         if (left_wrist_x > 400 && right_wrist_x < 200) { 
+    //             command = 74 //Pose 1
+    //         }
+    //     }
+
+    //     if (left_wrist_y < -400 && right_wrist_y > 0) {
+    //         if (left_wrist_x < 200 && right_wrist_x > 400) {
+    //             command = 75 //Pose 2
+    //         }
+    //     }
+    //     return command;
+    // },
 
     show: function (frame) {
         console.log(frame);
     }
 };
 
+// function sendPoseCommand(command) {
+//     switch (command) {
+//       case 73:
+//         answer = 'exit';
+//         break;
+
+//       case 74:
+//         answer = 'yes';
+//         break;
+
+//       case 75:
+//         answer = 'no';
+//         break;
+//     }
+//     console.log(answer);
+//   }
