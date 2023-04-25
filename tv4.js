3006d81c27df36076c07a487c01fb406a2bbf2df
jman1965 <76714921+jman1965@users.noreply.github.com>
2023-04-25 13:51:35 -0400

    done?

--- tv4.js
+++ tv4.js
@@ -29,10 +29,80 @@ var frames = {
         frames.socket.onmessage = function (event) {
             frames.show(JSON.parse(event.data));
         }
+
     },
 
+
     show: function (frame) {
-        console.log(frame);
+        var command = get_pose_command(frame);
+        //console.log(command)
+        if (command !== null) {
+            sendPoseCommand(command);
+        }
+        if(frame.people.length == 1){
+            one_person = 1;
+        } else {
+            one_person = 0;
+        }
+        if(global_answer == 'yes'){
+            window.location.href="TV-6-7.html";
+        }
+        if(global_answer == 'exit'){
+            window.location.href="TV-10.html";
+        }
+        //console.log(frame);
     }
 };
+var global_answer;
+function sendPoseCommand(command) {
+    answer = "test";
+    switch (command) {
+      case 73:
+        answer = 'exit';
+        break;
+
+      case 74:
+        answer = 'yes';
+        break;
+
+      case 75:
+        answer = 'no';
+        break;
+    }
+    global_answer = answer;
+  }
+
+function get_pose_command(frame) {
+
+    if (frame.people.length < 1) {
+      return null;
+    }
+
+    var head_y = (frame.people[0].joints[26].position.y) * -1;
+    var right_h_y = (frame.people[0].joints[15].position.y) * -1;
+    var left_h_y = (frame.people[0].joints[8].position.y) * -1;
+
+    var right_hand_up = 0;
+    var left_hand_up = 0;
+
+    if(right_h_y - head_y > 0){
+        right_hand_up = 1;
+    }
+
+    if(left_h_y - head_y > 0){
+        left_hand_up = 1;
+    }
+
+    if(right_hand_up && !left_hand_up){
+        return 75; 
+    } else if (left_hand_up && !right_hand_up){
+
+        return 74;
+    } else if(left_hand_up && right_hand_up){
+        return 73;
+    } else {
+        return null;
+    }
+}
+
 
--- tv67.js
+++ tv67.js
@@ -58,10 +58,103 @@ var frames = {
         frames.socket.onmessage = function (event) {
             frames.show(JSON.parse(event.data));
         }
+
     },
 
+
     show: function (frame) {
-        console.log(frame);
+        var command = get_pose_command(frame);
+        //console.log(command)
+        if (command !== null) {
+            sendPoseCommand(command);
+        }
+        if(frame.people.length == 1){
+            one_person = 1;
+        } else {
+            one_person = 0;
+        }
+        if(global_answer == 'yes'){
+            answers[index][0]++;
+
+          index++;
+          if (index >= questions.length) {
+            index = 0;
+          }
+          localStorage.setItem("answers", JSON.stringify(answers));
+          localStorage.setItem("index", index);
+          question.text(questions[index]);  
+
+          window.location.href="TV-8.html";
+        }
+        if(global_answer == 'exit'){
+            localStorage.setItem("index", index);
+
+            window.location.href="TV-10.html";
+        }
+        if(global_answer == 'no'){
+            answers[index][1]++;
+            index++;
+            if (index >= questions.length) {
+              index = 0;
+            }
+            localStorage.setItem("answers", JSON.stringify(answers));
+            localStorage.setItem("index", index);
+            question.text(questions[index]);
+  
+            window.location.href="TV-8.html";
+        }
+        //console.log(frame);
     }
 };
+var global_answer;
+function sendPoseCommand(command) {
+    answer = "test";
+    switch (command) {
+      case 73:
+        answer = 'exit';
+        break;
+
+      case 74:
+        answer = 'yes';
+        break;
+
+      case 75:
+        answer = 'no';
+        break;
+    }
+    global_answer = answer;
+  }
+
+function get_pose_command(frame) {
+
+    if (frame.people.length < 1) {
+      return null;
+    }
+
+    var head_y = (frame.people[0].joints[26].position.y) * -1;
+    var right_h_y = (frame.people[0].joints[15].position.y) * -1;
+    var left_h_y = (frame.people[0].joints[8].position.y) * -1;
+
+    var right_hand_up = 0;
+    var left_hand_up = 0;
+
+    if(right_h_y - head_y > 0){
+        right_hand_up = 1;
+    }
+
+    if(left_h_y - head_y > 0){
+        left_hand_up = 1;
+    }
+
+    if(right_hand_up && !left_hand_up){
+        return 75; 
+    } else if (left_hand_up && !right_hand_up){
+
+        return 74;
+    } else if(left_hand_up && right_hand_up){
+        return 73;
+    } else {
+        return null;
+    }
+}
 
